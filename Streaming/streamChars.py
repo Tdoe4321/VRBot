f18b3548d482e2c60b2002f99e2646ee32f73750
Tyler Gragg <tdoe3@comcast.net>
2019-04-05 11:19:04 -0400

    added streamChars

---
+++ Streaming/streamChars.py
@@ -0,0 +1,20 @@
+import base64
+import zmq
+
+context = zmq.Context()
+footage_socket = context.socket(zmq.PUB)
+footage_socket.setsockopt(zmq.CONFLATE, 1)
+#footage_socket.connect('tcp://192.168.1.141:5555')
+footage_socket.bind('tcp://*:55555')
+#camera = cv2.VideoCapture(0)  # init the camera
+
+#camera.set(3, 640)
+#camera.set(4, 480)
+
+while True:
+    try:
+        #text = base64.b64encode("hello world")
+        footage_socket.send_string("Hello World")
+
+    except KeyboardInterrupt:
+        break
