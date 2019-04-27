c8cf12f4494ded53f1306f7b958c412a70029882
Tyler Gragg <tdoe3@comcast.net>
2019-04-27 11:35:54 -0400

    testing new compression methods

--- Streaming/pushVideoPullCommandsCompress.py
+++ Streaming/pushVideoPullCommandsCompress.py
@@ -2,7 +2,6 @@ import base64
 import cv2
 import zmq
 import serial
-import zlib
 
 context = zmq.Context()
 footage_socket = context.socket(zmq.DEALER)
@@ -21,18 +20,23 @@ s1.baudrate = rate
 
 msg = None
 
+encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 95]
+
 def rescale_frame(frame, percent=75):
-    width = int(frame.shape[1] * percent/ 100)
-    height = int(frame.shape[0] * percent/ 100)
-    dim = (width, height)
-    return cv2.resize(frame, dim, interpolation =cv2.INTER_AREA)
+    #width = int(frame.shape[1] * percent/ 100)
+    #height = int(frame.shape[0] * percent/ 100)
+    #dim = (width, height)
+    float my_percent = percent / 100
+    return cv2.resize(frame, (0,0), fx=my_percent, fy=my_percent, interpolation =cv2.INTER_AREA)
 
 while True:
     try:
         grabbed, frame = camera.read()  # grab the current frame
-        #frame = rescale_frame(frame, 50)
-        encoded, buffer = cv2.imencode('.jpg', frame)
-        jpg_as_text = base64.b64encode(zlib.compress(buffer,6))
+        #frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        frame = rescale_frame(frame, )
+        #encoded, buffer = cv2.imencode('.jpg', frame)
+        encoded, buffer = cv2.imencode('.jpg', frame, encode_param)
+        jpg_as_text = base64.b64encode(buffer)
         footage_socket.send(jpg_as_text)
         msg = footage_socket.recv().decode("utf-8")
         if(not msg):
